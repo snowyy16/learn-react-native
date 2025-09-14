27e6f1e22da213169dd665ad19aaa11fe9c80a99
snowyy16 <huynhson532004@gmail.com>
2025-09-14 17:00:20 +0700

    view style text

--- App.tsx
+++ App.tsx
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world with Snow!</Text>
-      <StatusBar style="auto" />
+      <View >
+        <Text style={styles.header}>Land dau su dung</Text>
+      </View>
+      <Text style={styles.hello1}>Hello world 1</Text>
+      <Text>Hello world 2!</Text>
     </View>
   );
 }
@@ -16,5 +19,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap:10
   },
+  hello1:{
+    color: 'blue',
+    fontSize:40,
+    borderColor:'green',
+    borderRadius:8,
+    borderWidth:1,
+    gap:10
+  },
+  header:{
+    fontFamily:"bold",
+    fontSize:50,
+    padding:5,
+    justifyContent:"center",
+    alignItems:'center',
+    color:'cyan'
+  }
 });
