1226652c91dd6200d797a09f7c6fbbf6a7b7207e
snowyy16 <huynhson532004@gmail.com>
2025-09-15 14:17:57 +0700

    useState beginner

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
+import { use, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
+
+  const [number,setNumber] = useState(0)
+
   return (
     <View style={styles.container}>
-      <View >
-        <Text style={styles.header}>Land dau su dung</Text>
+      <Text style={styles.hello1}>
+         count = {number}
+      </Text>
+      <View>
+        <Button color={"red"} 
+        title='increase' onPress={()=>setNumber(number+1)}/>
+
+        
       </View>
-      <Text style={styles.hello1}>Hello world 1</Text>
-      <Text>Hello world 2!</Text>
     </View>
   );
 }
@@ -25,16 +32,8 @@ const styles = StyleSheet.create({
     color: 'blue',
     fontSize:40,
     borderColor:'green',
-    borderRadius:8,
-    borderWidth:1,
+    fontWeight:600,
     gap:10
   },
-  header:{
-    fontFamily:"bold",
-    fontSize:50,
-    padding:5,
-    justifyContent:"center",
-    alignItems:'center',
-    color:'cyan'
-  }
+  
 });
